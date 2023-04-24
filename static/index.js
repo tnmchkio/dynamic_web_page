2d7b8b9c5bd4ffc8a1a24bc6ef81e932058179ab
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2023-04-24 15:46:27 -0500

    small update

--- static/index.js
+++ static/index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.querySelector("#submit").disabled = true;
 });
 
+//	commentary for test
+
 function validate() {
 	document.querySelector("#submit").disabled  = false;
 	document.querySelectorAll('.check_empty').forEach(input => {
